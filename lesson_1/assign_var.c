60e320ad72892f1a98bfc2e5281af30927642da9
khadim <kramoth@udm.ac.mu>
2025-04-08 21:18:31 +0400

    adding var assignation example and const example

---
+++ lesson_1/assign_var.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int nombre_de_jour;
+	nombre_de_jour=42;
+	/* ou bien 
+	int nombre_de_jour=90;
+	*/
+	printf(" J- %d  avant les vacances\n", nombre_de_jour);
+}
---
+++ lesson_1/const_example.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main()
+{
+	const int SOFTWARE_VERSION=3;
+	printf(" you are on the software version %d\n", SOFTWARE_VERSION);
+}
